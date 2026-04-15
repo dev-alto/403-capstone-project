2bd15109005936e51720e0f451c7bb180d3f41f8
devalto <51037255+dev-alto@users.noreply.github.com>
2026-04-15 15:42:04 -0700

    feat: init routes for /intinerary and /places

--- application/src/app/api/itinerary/route.ts
+++ application/src/app/api/itinerary/route.ts
@@ -1,2 +1,8 @@
 // This will be for the API to generate 
 // the plan
+
+// this is used for the locations
+
+export async function GET() {
+  return Response.json({ message: 'this the api/itinerary/ endpoint' })
+}
