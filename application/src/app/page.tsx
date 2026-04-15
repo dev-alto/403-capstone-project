78a103eba945d179656b1a6e044b43eda29fffb5
devalto <51037255+dev-alto@users.noreply.github.com>
2026-04-15 15:40:59 -0700

    feat: init front end hub design

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -10,9 +10,23 @@ const testPlaces = [
 
 export default function Home() {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Map Test</h1>
-      <MapView places={testPlaces} />
+    <main className="w-full h-screen flex flex-col">
+      <h1 className="w-full h-10 p-6 flex items-center justify-between text-2xl font-bold">
+        <p>
+          Itinerary Builder Concept
+        </p>
+        <p>-</p>
+      </h1>
+      <div className="w-full h-full">
+        <MapView places={testPlaces} />
+
+        <div className="z-10 absolute w-full bottom-0 flex flex-col items-center">
+          <div className="w-1/2 h-32 flex flex-col items-center p-4 bg-white rounded-t-2xl">
+            <p className="font-bold">Describe your Journey</p>
+            <input className="bg-neutral-200" type="text"></input>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
